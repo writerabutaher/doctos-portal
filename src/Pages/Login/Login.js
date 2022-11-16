a82aca0a18eb77c1b3dcab45aee4a0bf4baf03ad
Abu Taher <writerabutaher@gmail.com>
2022-11-16 12:38:27 +0600

    appointment

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
-    return (
-        <div>
 
-        </div>
+    const { register, handleSubmit } = useForm();
+    const [data, setData] = useState("");
+
+    return (
+        <>
+            <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                {/* <Header /> */}
+                <input {...register("firstName")} placeholder="First name" />
+                <select {...register("category", { required: true })}>
+                    <option value="">Select...</option>
+                    <option value="A">Option A</option>
+                    <option value="B">Option B</option>
+                </select>
+                <textarea {...register("aboutYou")} placeholder="About you" />
+                <p>{data}</p>
+                <input type="submit" />
+            </form>
+        </>
     );
 };
 
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Appointment from "../Pages/Appointment/Appointment";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -19,6 +21,14 @@ const router = createBrowserRouter([
             {
                 path: '/appointment',
                 element: <Appointment></Appointment>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             },
         ]
     }
